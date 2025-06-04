9d3109342f777d9a2d07e732fd4e919851f738c9
Dany De La Rosa <dev.dany@outlook.com>
2025-06-04 11:04:26 -0400

    Declare the characters that will be used in the console game as constants

---
+++ maze.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const character_Icon = '𐙬'
+const topIntersection  = '╦'
+const rightEdge = '╣'
+const leftEdge = '╠'
+const middleIntersection = '╬'
+const bottomIntersection = '╩'
+const topLeftCorner = '╔'
+const topRightCorner = '╗'
+const bottomLeftCorner = '╚'
+const bottomRightCorner = '╝'
+const horizontalEdge = '═'
+const verticalEdge = '║'
+
