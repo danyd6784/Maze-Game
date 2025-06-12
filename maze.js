eaa24778e01d7e33d2ae2e4daf49dd56d4acd18f
danyd6784 <104733422+danyd6784@users.noreply.github.com>
2025-06-12 09:57:16 -0400

    Write createGrid function which creates equilateral two dimensional array and test with main function

--- maze.js
+++ maze.js
@@ -13,3 +13,17 @@ const bottomRightCorner = '╝'
 const horizontalEdge = '═'
 const verticalEdge = '║'
 
+//Create an equilateral 2d array and populate with arbitrary character
+function createGrid(gridDimension){
+    let gridArray = [];
+    for (let i = 0; i < gridDimension; i++){
+        //push new array filled with Arbitrary character to every slo
+        gridArray.push(Array(gridDimension).fill("O"));
+    }
+    return gridArray;
+}
+
+function main(){
+    console.log(createGrid(4));
+}
+main()
