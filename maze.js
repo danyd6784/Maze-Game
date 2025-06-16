5b5c64c38eb19341612a9db1002c17680ac02110
danyd6784 <104733422+danyd6784@users.noreply.github.com>
2025-06-16 13:40:03 -0400

    Add two supporting functions to generate x and y coordinates

--- maze.js
+++ maze.js
@@ -14,6 +14,10 @@ const horizontalEdge = '═'
 const verticalEdge = '║'
 
 //Create an equilateral 2d array and populate with arbitrary character
+//Note: this is assuming that the grid will always be a square - a neat enhancement would be one that locks the dimensions
+//of the console window to a specified height and width and always works off of that. 
+//Of course this would be a bit of a gamble because of higher screen resolutions being a bitch to characters and windows
+//but maybe would be a great addition to another version of the app
 function createGrid(gridDimension){
     let gridArray = [];
     for (let i = 0; i < gridDimension; i++){
@@ -23,7 +27,16 @@ function createGrid(gridDimension){
     return gridArray;
 }
 
+function generateRandomX(gridDimension){
+    return Math.floor(Math.random() * gridDimension);
+}
+
+function generateRandomY(gridDimension){
+    return Math.floor(Math.random() * gridDimension);
+}
+
 function main(){
     console.log(createGrid(4));
 }
+
 main()
